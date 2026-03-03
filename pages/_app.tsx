c001e1f799780038ab1d82356f65ec45abdea186
tanismehta_deloitte <tanismehta@deloitte.com>
2026-03-03 22:06:36 +0530

    added analytics functionality for vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { GeoProvider } from '@/context/GeoContext'
 import { CartProvider } from '@/context/CartContext'
+import { Analytics } from "@vercel/analytics/next"
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <CartProvider>
         <Component {...pageProps} />
       </CartProvider>
+      <Analytics />
     </GeoProvider>
   )
 }
